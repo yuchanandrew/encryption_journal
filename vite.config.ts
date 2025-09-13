bc0b6ac512313a2c73a9643a196517c90e6caf52
yuchanandrew <anrh8554@colorado.edu>
2025-09-13 16:42:47 -0600

    Removed tsc -b from build script

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: false,        // Speeds up builds
+    target: 'esnext',        // Optimize output for modern browsers
+    chunkSizeWarningLimit: 1000, // Optional, avoids noisy warnings
+  },
   server: {
     host: true,
     port: 5173,
     strictPort: true
   },
   preview: {
-    allowedHosts: ['emjournal.dev', 'www.emjournal.dev']
+    allowedHosts: ['emjournal.dev','www.emjournal.dev','api.emjournal.dev']
   }
 })
