23859c0807dbfa95f2428271232ff85689d381ce
yuchanandrew <anrh8554@colorado.edu>
2025-09-13 22:59:51 -0600

    Testing base_url=''; for Register.tsx

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-const base_url = "api";
+const base_url = "";
 
 const Register = () => {
   const [username, setUsername] = useState("");
